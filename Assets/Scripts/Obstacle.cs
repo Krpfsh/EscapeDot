d5d7a62117af57d0c6be272c7bcea0f122c8125f
Krpfsh <nik200020@mail.ru>
2024-02-17 05:19:59 +0300

    BugFix

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -6,30 +6,27 @@ public class Obstacle : MonoBehaviour
     private float currentRotateSpeed;
 
     [SerializeField] private float _minRotateTime, _maxRotateTime;
-
     private float rotateTime;
     private float currentRotateTime;
 
     private void Awake()
     {
         currentRotateTime = 0f;
-        currentRotateSpeed = _minRotateSpeed + (_maxRotateSpeed - _minRotateSpeed) * 0.1f * Random.RandomRange(0, 11);
-        rotateTime = _minRotateSpeed + (_maxRotateSpeed - _minRotateSpeed) * 0.1f * Random.RandomRange(0, 11);
-
+        currentRotateSpeed = _minRotateSpeed + (_maxRotateSpeed - _minRotateSpeed) * 0.1f * Random.Range(0, 11);
+        rotateTime = _minRotateTime + (_maxRotateTime - _minRotateTime) * 0.1f * Random.Range(0, 11);
         currentRotateSpeed *= Random.Range(0, 2) == 0 ? 1f : -1f;
-
     }
 
     private void Update()
     {
         currentRotateTime += Time.deltaTime;
+
         if (currentRotateTime > rotateTime)
         {
             currentRotateTime = 0f;
-            currentRotateSpeed = _minRotateSpeed + (_maxRotateSpeed - _minRotateSpeed) * 0.1f * Random.RandomRange(0, 11);
-            rotateTime = _minRotateSpeed + (_maxRotateSpeed - _minRotateSpeed) * 0.1f * Random.RandomRange(0, 11);
+            currentRotateSpeed = _minRotateSpeed + (_maxRotateSpeed - _minRotateSpeed) * 0.1f * Random.Range(0, 11);
+            rotateTime = _minRotateTime + (_maxRotateTime - _minRotateTime) * 0.1f * Random.Range(0, 11);
             currentRotateSpeed *= Random.Range(0, 2) == 0 ? 1f : -1f;
-            Debug.Log(currentRotateSpeed);
         }
     }
 
