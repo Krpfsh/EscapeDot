f84cb9028648623703d0ba6c6625b96c8b9c062f
Krpfsh <nik200020@mail.ru>
2024-02-23 04:58:42 +0300

    bug fix

---
+++ Assets/RestartGameMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGameMenu : MonoBehaviour
+{
+    public void RestartButton()
+    {
+
+    }
+}
--- Assets/Scripts/GamePlayManager.cs
+++ Assets/Scripts/GamePlayManager.cs
@@ -8,13 +8,15 @@ public class GameplayManager : MonoBehaviour
     private bool hasGameFinished;
 
     [SerializeField] private TMP_Text _scoreText;
+    [SerializeField] private TMP_Text _scoreTextFinal;
+    [SerializeField] private TMP_Text _bestScoreTextFinal;
 
     private float score;
     private float scoreSpeed;
     private int currentLevel;
 
     [SerializeField] private List<int> _levelSpeed, _levelMax;
-
+    [SerializeField] private GameObject _gameOverObj;
     private void Awake()
     {
         GameManager.Instance.IsInitialized = true;
@@ -46,6 +48,15 @@ public class GameplayManager : MonoBehaviour
         hasGameFinished = true;
         GameManager.Instance.CurrentScore = (int)score;
 
+        int currentScore = GameManager.Instance.CurrentScore;
+        int highScore = GameManager.Instance.HighScore;
+
+        if (highScore < currentScore)
+        {
+            GameManager.Instance.HighScore = currentScore;
+        }
+        _scoreTextFinal.text = GameManager.Instance.CurrentScore.ToString();
+        _bestScoreTextFinal.text = GameManager.Instance.HighScore.ToString();
         StartCoroutine(GameOver());
     }
 
@@ -53,7 +64,11 @@ public class GameplayManager : MonoBehaviour
     private IEnumerator GameOver()
     {
         yield return new WaitForSeconds(2f);
-        GameManager.Instance.GotoMainMenu();
+        _gameOverObj.SetActive(true);
+    }
+    public void RestartButton()
+    {
+        GameManager.Instance.GotoGameplay();
     }
 
 
